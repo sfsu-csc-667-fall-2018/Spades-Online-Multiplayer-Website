cd983d2638a87b7585f4265d0a7fda5f79d44f4c
Robert Ian Clarkson <robert.ian.clarkson@gmail.com>
2020-04-09 13:56:23 -0700

    Created POST method for registration Content of registration is now sved to the db

--- spades-server/routes/pages/index.js
+++ spades-server/routes/pages/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db');
 
 /* GET home page. */
 router.get('/', (request, response, next) => {
@@ -12,6 +13,26 @@ router.get('/users/register', (request, response, next) => {
   response.render('unauthenticated/register', {
     title: 'Register'
   })
+});
+
+router.post('/users/register', (request, response, _) => {
+  db.any(
+    `INSERT INTO player (first_name, last_name, username, email, password) VALUES (
+        '${request.body.first_name}', 
+        '${request.body.last_name}', 
+        '${request.body.username}', 
+        '${request.body.email}',
+        '${request.body.password}'
+      )`
+  )
+  .then( results => response.json( results ) )
+  .catch( error => {
+    console.log( error )
+    response.json({ error })
+  });
+  response.render('index', {
+    title: 'Index'
+  });
 });
 
 router.get('/users/login', (request, response, next) => {

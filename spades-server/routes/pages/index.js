b4d98b79caabb801febd25fda9697d9727b21e5e
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-04-09 21:56:42 -0700

    authentication to lobby page successful

--- spades-server/routes/pages/index.js
+++ spades-server/routes/pages/index.js
@@ -5,17 +5,16 @@ const db = require('../../db');
 
 /* GET login page or go to lobby if auth */
 router.get('/', (request, response) => {
-  if(request.isAuthenticated()) {
-    response.redirect('/lobby');
-  }else{
-    response.render('login');
-  }
+    if (request.isAuthenticated()) {
+        response.redirect('/lobby');
+    } else {
+        response.render('login');
+    }
 });
 
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/lobby',
-  failureRedirect: '/'
-  })
-);
+    successRedirect: '/lobby',
+    failureRedirect: '/'
+}));
 
 module.exports = router;

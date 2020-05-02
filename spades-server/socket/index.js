ed674f9316c115e6334463b84e6513eac9437cff
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-05-02 15:27:15 -0700

    chatting in lobby

--- spades-server/socket/index.js
+++ spades-server/socket/index.js
@@ -7,15 +7,18 @@ const init = (app, server) => {
   app.set('io', io);
 
   io.on('connection', socket => {
-    console.log('connected to socket io');
+    console.log('connected to socket io')
 
     socket.on('disconnect', data => {
         console.log('disconnected from socket io');
-    });
+    })
 
-    socket.on( ENTERED, data => io.emit( ENTRY_MESSAGE, data ));
-    socket.on( CHAT, data => io.emit( SEND_MESSAGE, data ));
-    socket.on( TYPING, data => socket.broadcast.emit(TYPING_MESSAGE, data) );
+    socket.on('send message', (message) => {
+      io.emit('send message', message)
+    });
+    //socket.on( ENTERED, data => io.emit( ENTRY_MESSAGE, data ));
+    //socket.on( CHAT, data => io.emit( SEND_MESSAGE, data ));
+    //socket.on( TYPING, data => socket.broadcast.emit(TYPING_MESSAGE, data) );
   });
 };
 

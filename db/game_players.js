362f0f114f83ee098ca563952b7f18b9cfda58d7
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-05-07 00:05:51 -0700

    Added 'game_players' functions from db_cards

---
+++ db/game_players.js
@@ -0,0 +1,28 @@
+const db = require('./index');
+
+const getEmptyPostion = ( position_arr ) => {
+    if(position_arr.length == 0)  return 0;
+    if(!position_arr.some(element => element.position === 0)) return 0;
+    if(!position_arr.some(element => element.position === 1)) return 1;
+    if(!position_arr.some(element => element.position === 2)) return 2;
+    if(!position_arr.some(element => element.position === 3)) return 3;
+}
+
+const getPositions = ( game_id ) => {
+    return db.manyOrNone(`SELECT position from game_players WHERE game_id=${ game_id }`);
+};
+
+const addPlayer = async ( game_id, player_id ) => {
+    var position_arr = await getPositions( game_id );
+    var emptyPos = getEmptyPostion( position_arr );
+    return db.none(`INSERT INTO game_players (game_id, player_id, position) VALUES (
+        ${ game_id },
+        ${ player_id },
+        ${ emptyPos }
+    )`);
+};
+
+module.exports = {
+    // getPositions,
+    addPlayer
+};

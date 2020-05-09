e63c48c0ebdd4bf10526ec3af6a96dc2fea9e810
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-05-09 16:25:32 -0700

    fixed bug in 'initScores'

--- db/game.js
+++ db/game.js
@@ -33,7 +33,7 @@ const updateNumPlayers = (gameId) => {
 }
 
 const initScores = (gameId) => {
-  return db.one(`INSERT INTO scores (
+  return db.none(`INSERT INTO scores (
     game_id, books_a, books_b, bags_a, bags_b, bets_a, bets_b, points_a, points_b
     ) VALUES ('${gameId}', 0, 0, 0, 0, 0, 0, 0, 0)`);
 };

b78d77605b44f44ad38f91caad6e951b63e95703
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-05-09 19:41:22 -0700

    Drafted listing cards a player in a game room

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,1 +1,10 @@
 const gameSocket = io('/game');
+
+
+gameSocket.on('display cards', cards => {
+    console.log(cards);
+    $('#cards').empty();
+    for(let i = 0; i < cards.length; i++) {
+        $('#cards').append(`<li>${cards[i].image}</li>`);
+    }
+});

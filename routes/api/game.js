4c5f5da7d263425e1f3debd98eed9b1976ab77c6
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-05-09 19:40:49 -0700

    Drafted creating a deck for a game room once full

--- routes/api/game.js
+++ routes/api/game.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const isAuthenticated = require('../../config/passport/isAuthenticated');
 const io = require('../../socket');
 const gameSocket = io.of('/game');
+const gameLogic = require('./game_logic');
+
 const game = require('../../db/game');
-const playerTable = require('../../db/game_players');
+const cards = require('../../db/game_cards');
+const players = require('../../db/game_players');
 
 let user, game_id;
 
@@ -15,7 +18,6 @@ router.get('/', isAuthenticated, (request, response) => {
 router.get('/:game_id', isAuthenticated, (request, response) => {
   user = request.user;
   game_id = request.params.game_id;
-
   game.getGameRoom(game_id)
     .then(results => {
       if(results === undefined || results.length === 0){
@@ -43,6 +45,25 @@ gameSocket.on('connection', socket => {
   socket.join(game_id);
 
   //sockets api functions go here
+
+  /* if game room is full create the deck */
+  gameLogic.gameReady(game_id).then((hasPlayers) => {
+    console.log('hasPlayers? : ' + hasPlayers);
+    if(hasPlayers) {
+      cards.deckReady(game_id).then((hasDeck) => {
+        console.log('hasDeck? : ' + hasDeck);
+        if(!hasDeck) {
+          players.getPlayers(game_id).then((players) => {
+            console.log('***players***');
+            console.log(players);
+            cards.createDeck(game_id, players).then(() => {
+              console.log("Created Deck");
+            });
+          });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;

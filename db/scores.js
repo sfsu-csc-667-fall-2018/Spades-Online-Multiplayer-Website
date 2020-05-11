922cced08e93a2eb8874da53a7c2ba7d396c9cce
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-05-11 15:36:39 -0700

    added 'initScores()'

--- db/scores.js
+++ db/scores.js
@@ -29,3 +29,14 @@ const getTeam = (gameId, playerId) => {};
 const getScoreBoard = (gameId) => {
   return db.one(`SELECT * FROM scores WHERE game_id = '${gameId}'`);
 };
+
+const initScore = (game_id) => {
+  return db.none(`INSERT INTO scores (game_id, books_a, books_b, bags_a, bags_b, bets_a, bets_b, points_a, points_b) VALUES (
+    ${game_id},
+    0, 0, 0, 0, 0, 0, 0, 0
+  );`)
+}
+
+module.exports = {
+  initScore
+}

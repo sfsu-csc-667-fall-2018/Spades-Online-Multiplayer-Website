7e84dcb9bb974d5c92c29e51294b1e48c9f5bca7
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-05-11 18:33:11 -0700

    played cards rendering to game

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -10,14 +10,22 @@ $(window).on('load', () => {
 gameSocket.on('display cards', cards => {
   console.log(cards);
     $('#cards').empty();
-    for(let i = 0; i < cards.length; i++) {
+    for(let i = 0; i < cards.length; i++) { // every card in deck
       if(cards[i].player_id == id && cards[i].card_order > 0) {
+        /* card belongs to user */
         $('#cards').append(`
           <li>
             ${cards[i].image}
             <button type=submit name="card_id" value=${cards[i].id}>
               Play '${cards[i].name}'
             </button>
+          </li>
+        `);
+      } else if(cards[i].card_order == 0) {
+        /* card belongs to table */
+        $('#table').append(`
+          <li>
+            ${cards[i].image}
           </li>
         `);
       }

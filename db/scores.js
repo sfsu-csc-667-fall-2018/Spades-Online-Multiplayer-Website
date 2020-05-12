2b5e45758bf994d42eb63bc948111ceb5fae0052
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-05-11 21:56:58 -0700

    scores table queries

--- db/scores.js
+++ db/scores.js
@@ -14,22 +14,56 @@ const getScores = (gameId) => {
     .catch(error => { console.log(error) }); 
 };
 
-const getBooks = (gameId) => {};
+const getBooks = (gameId, team) => {
+  if(team == 1){
+    return db.one(`SELECT books_a FROM scores WHERE game_id = ${gameId}`);
+  }else if(team == 2){
+    return db.one(`SELECT books_b FROM scores WHERE game_id = ${gameId}`);
+  };
+};
 
-const getBags = (gameId) => {};
+const getBags = (gameId, team) => {
+  if(team == 1){
+    return db.one(`SELECT bags_a FROM scores WHERE game_id = ${gameId}`);
+  }else if(team == 2){
+    return db.one(`SELECT bags_b FROM scores WHERE game_id = ${gameId}`);
+  };
+};
 
-const addBet = (gameId, team) => {};
+const addBet = (gameId, team, val) => {
+  if(team == 1){
+    return db.none(`UPDATE scores SET bets_a = (bets_a + ${val}) WHERE game_id = ${gameId}`);
+  }else if(team == 2){
+    return db.none(`UPDATE scores SET bets_b = (bets_b + ${val}) WHERE game_id = ${gameId}`);
+  };
+};
 
-const addBook = (gameId, team) => {};
+const addBook = (gameId, team) => {
+  if(team == 1){
+    return db.none(`UPDATE scores SET books_a = (books_a + 1) WHERE game_id = ${gameId}`);
+  }else if(team == 2){
+    return db.none(`UPDATE scores SET bets_b = (books_b + 1) WHERE game_id = ${gameId}`);
+  };
+};
 
-const addScore = (gameId, team) => {};
+//add scores when round is tallied, val1 = team 1, val2 = team 2
+const addScore = (gameId, val1, val2) => {
+  return db.none(`UPDATE scores 
+    points_a = (points_a + ${val1}), 
+    points_b = (points_b + ${val2}) 
+    WHERE game_id = ${gameId}`);
+};
 
-const getTeam = (gameId, playerId) => {};
 
 const getScoreBoard = (gameId) => {
   return db.any(`SELECT * FROM scores WHERE game_id = '${gameId}'`);
 };
 
 module.exports = {
-  getScoreBoard
+  getScoreBoard,
+  getBags,
+  getBooks,
+  addBet,
+  addBook,
+  addScore
 };

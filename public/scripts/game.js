e4e7e659028a8d9fb82ba09503b87b6c12c69cb6
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-05-13 21:11:34 -0700

    game socket fix

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,14 +1,14 @@
 const gameSocket = io('/game');
 
-// let id, playerNames, num_players, playerBot, playerTop, playerLeft, playerRight,
-// orderBot, orderTop, orderLeft, orderRight, currentPlayer, currentTurn, selectCard = false, selectedCard = '0';
+let id, playerNames, num_players, playerBot, playerTop, playerLeft, playerRight,
+  orderBot, orderTop, orderLeft, orderRight, currentPlayer, currentTurn, selectCard = false, selectedCard = '0';
 
 
-// $(window).on('load', () => {
-//   id = $('#user_id').val();
-//   console.log('trying to load cards');
-//   gameSocket.emit('get hand');
-// });
+$(window).on('load', () => {
+  id = $('#user_id').val();
+  console.log('trying to load cards');
+   gameSocket.emit('get hand');
+});
 
 // gameSocket.on('display cards', cards => {
 //     console.log(cards);
@@ -23,31 +23,31 @@ const gameSocket = io('/game');
 //     }
 // });
 
-// gameSocket.on('update score' , data => {
+gameSocket.on('update score', data => {
 
-//   console.log('client displaying score');
+  console.log('client displaying score');
 
-//   //$('#team1-books').empty();
-//   //$('#team1-bags').empty();
-//   //$('#team1-bets').empty();
-//   //$('#team1-score').empty();
-//   //$('#team2-books').empty();
-//   //$('#team2-bags').empty();
-//   //$('#team2-bets').empty();
-//   //$('#team2-score').empty();
+  //$('#team1-books').empty();
+  //$('#team1-bags').empty();
+  //$('#team1-bets').empty();
+  //$('#team1-score').empty();
+  //$('#team2-books').empty();
+  //$('#team2-bags').empty();
+  //$('#team2-bets').empty();
+  //$('#team2-score').empty();
 
-//   const { books_a, books_b, bags_a, bags_b, bets_a, bets_b, points_a, points_b } = data;
+  const { books_a, books_b, bags_a, bags_b, bets_a, bets_b, points_a, points_b } = data;
 
-//   $('#team1-books').text('Books: ' + books_a);
-//   $('#team1-bags').text('Bags: ' + bags_a);
-//   $('#team1-bets').text('Bet: ' + bets_a);
-//   $('#team1-score').text('Score:' + points_a);
-//   $('#team2-books').text('Books: ' + books_b);
-//   $('#team2-bags').text('Bags: ' + bags_b);
-//   $('#team2-bets').text('Bet: '+ bets_b);
-//   $('#team2-score').text('Score: ' + points_b);
-  
-// });
+  $('#team1-books').text('Books: ' + books_a);
+  $('#team1-bags').text('Bags: ' + bags_a);
+  $('#team1-bets').text('Bet: ' + bets_a);
+  $('#team1-score').text('Score:' + points_a);
+  $('#team2-books').text('Books: ' + books_b);
+  $('#team2-bags').text('Bags: ' + bags_b);
+  $('#team2-bets').text('Bet: ' + bets_b);
+  $('#team2-score').text('Score: ' + points_b);
+
+});
 
 // gameSocket.on('update players', data => {
   
@@ -71,7 +71,7 @@ const gameSocket = io('/game');
 
 // //gameSocket.on('get bets', () => {});
 
-// //helpers
+//helpers
 // const getBet = () => {
 //   let bet;
 //   var gameBox = $('#game-box');
@@ -80,10 +80,8 @@ const gameSocket = io('/game');
 //   while (!Number.isInteger(bet) && bet > 13 && bet < 0){
 //     gameBox.empty().append(
 //       $('<p>').append('Bet: ', betPrompt),
-//       submit);
+//     submit);
 //   };
-
-//   return bet;
-  
+//   return bet;  
 // }; 
 

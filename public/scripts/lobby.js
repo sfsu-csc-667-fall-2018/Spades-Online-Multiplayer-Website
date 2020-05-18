d205c70681a95a51db824815b8e7cbd628d86da0
Brooks <stanleyb@stanleyb-mbr.attlocal.net>
2020-05-17 21:09:25 -0700

    Lobby updated with css

--- public/scripts/lobby.js
+++ public/scripts/lobby.js
@@ -12,14 +12,18 @@ lobbySocket.on('display games', currentGames => {
 
     const { game_id, game_name, scores_id, num_players } = currentGames[i];
     
-    const str = 'Name: ' + game_name + ' Players: ' + num_players;
+    const str = 'Name: ' + game_name + ' Players: ' + num_players + '/4';
 
     if(num_players < 4){
       const form = $( 
         `<form method="POST" action="/lobby/joinGame">
-          <label>"${str}"</label>
-          <input type="hidden" name="game_id" value="${game_id}"></input>
-          <input type="submit" value="Join Game">
+          <div align="left">
+            <br>
+            <input class="btn-small waves-effect waves-dark" type="submit" value="Join" style="float: none; "> 
+            <label>${str}</label>
+            <input type="hidden" name="game_id" value="${game_id}"></input> 
+            
+          </div>
         </form>`
       );
 
